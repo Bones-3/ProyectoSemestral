52939fc479d64691e61515bb51b2d4c97f9af5f8
Fabián Jara <fa.jarao@duocuc.cl>
2026-05-08 00:49:59 -0400

    Se agrega el ms-pedido al proyecto, y se deja de trabajar en cliente para realizar un analisis de su desarrollo

---
+++ ms-cliente/src/main/java/com/restaurant/ms_cliente/model/Cliente.java
@@ -0,0 +1,35 @@
+package com.restaurant.ms_cliente.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cliente")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_cliente")
+    private Long id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombreProducto;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "correo")
+    private String correo;
+
+
+
+}
